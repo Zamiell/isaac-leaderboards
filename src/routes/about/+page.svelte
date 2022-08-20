7b496afa1b1b86ed4cff5ca33b906be1f166ee62
James <5511220+Zamiell@users.noreply.github.com>
2022-08-20 13:30:00 -0400

    feat: adding pingo's rules

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -46,10 +46,18 @@
           <h2 class="section-header">Rules</h2>
           <ol class="section-text">
             <li>
-              During each run, you must both complete the goal and see the
-              credits. (For example, if you complete the goal but die before
-              jumping into the chest, your streak is not incremented and it is
-              counted the same as if you died to the boss.)
+              You must start the run as the character from the category you are
+              playing. If you accidently start a run as a different character
+              from the streak you are working on, you are allowed to exit the
+              game and restore a previously backed up save file in order to
+              preserve the streak count.
+            </li>
+            <li>
+              You must trigger the ending cinematic of the goal for the category
+              you are playing. (For example, if you complete the goal but die
+              before jumping into the chest, your streak is not incremented and
+              it is counted the same as if you died to the boss. Ending a run at
+              Cathedral or Sheol is not allowed.)
             </li>
             <li>Every run must be played on the "hard" difficulty.</li>
             <li>
@@ -61,7 +69,9 @@
               gameplay advantage. (For example, you are not allowed to pause in
               the middle of a fight to think about what to do next, go back to
               the menu to escape damage, stop a restock machine from "double
-              rolling", and so on.)
+              rolling", delete champions, reset the Holy Mantle, and so on.) You
+              are allowed to save and quit and continue if it the only way to
+              recover from a softlock such as a bugged room.
             </li>
             <li>
               Every run must be streamed live on
@@ -80,7 +90,11 @@
               >
                 YouTube
               </a>. (For example, showing video recordings of your run after the
-              fact does not count.)
+              fact does not count.) You must also
+              <a
+                href="https://streamersplaybook.com/how-to-enable-vod-on-twitch/"
+                >enabled VODs</a
+              > so that administrators can review your runs after the fact.
             </li>
             <li>
               Any mods that affect the gameplay are forbidden. (For example,
@@ -97,8 +111,33 @@
                 rel="noopener noreferrer"
               >
                 Rebirth Item Tracker
-              </a> for this purpose. Otherwise, you can pause at the beginning and
+              </a> for this purpose. Otherwise, you can pause at the beginning or
               at the end of your run.)
+            </li>
+            <li>
+              Playing on PC is preferred, but you are allowed to play on any
+              other system if you want (such as the Nintendo Switch), as long as
+              the seeds would be indentical to what they would be on PC for
+              verification purposes.
+            </li>
+            <li>
+              You are allowed to end the run on another character than the one
+              you started with. (For example, you are allowed to take Judas'
+              Shadow and revive as Dark Judas.)
+            </li>
+            <li>
+              When your streak starts, the donation machine in the shop can be
+              at any value between 0 and 999. However, once the streak starts,
+              you are not allowed to change this value in any way other than by
+              donating coins to the machine and bombing the machine. (For
+              example, you are not allowed to edit the save file in manually
+              change the value.) You are allowed to bomb the donation machine in
+              the shop in order to get coins that were donated in a previous
+              run.
+            </li>
+            <li>
+              You are not allowed to use the debug console, Cheat Engine, or any
+              other way of modifying the game.
             </li>
           </ol>
         </div>
@@ -122,18 +161,28 @@
               </a> - This website was based on this resource.
             </li>
             <li>
+              - <a
+                href="https://github.com/Zamiell/isaac-save-installer/releases"
+                >100% save file installer</a
+              >.
+            </li>
+            <li>
               <a
                 href="https://isaacrankings.com/"
                 target="_blank"
-                rel="noopener noreferrer">Isaac Rankings</a
-              > - The most skilled Isaac players in the world participate in tournaments.
+                rel="noopener noreferrer"
+              >
+                Isaac Rankings
+              </a> - The most skilled Isaac players in the world participate in tournaments.
             </li>
             <li>
               <a
                 href="https://isaacracing.net/"
                 target="_blank"
-                rel="noopener noreferrer">Isaac Racing</a
-              > - Home of Racing+, a racing mod.
+                rel="noopener noreferrer"
+              >
+                Isaac Racing
+              </a> - Home of Racing+, a racing mod.
             </li>
           </ul>
         </div>
