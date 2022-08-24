224b02adcec5cebbc92b962a4ec66171734ac8b4
James <5511220+Zamiell@users.noreply.github.com>
2022-08-24 02:30:13 -0400

    refactor: rules into sections

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -23,9 +23,9 @@
     <div class="bg-gray-50 shadow sm:rounded-lg sm:overflow-hidden">
       <div class="divide-y divide-gray-200">
         <div class="px-4 py-5 sm:px-6">
-          <h2 class="section-header">Intro</h2>
+          <h2>Intro</h2>
           <p class="section-text">
-            The Binding of Isaac is a rougue-like, top-down shooter. The goal of
+            The Binding of Isaac is a rogue-like, top-down shooter. The goal of
             the game is to travel through 10 floors and defeat the boss. For the
             best players, doing this is easy. But how many times can it be done
             in a row?
@@ -39,15 +39,91 @@
     <div class="bg-gray-50 shadow sm:rounded-lg sm:overflow-hidden">
       <div class="divide-y divide-gray-200">
         <div class="px-4 py-5 sm:px-6">
-          <h2 class="section-header">Rules</h2>
-          <ol class="section-text">
+          <h2>Rules</h2>
+
+          <h3>Platform</h3>
+
+          <ul class="section-text">
+            <li>
+              You must play on PC, so that other people can replay your seeds to
+              verify your runs. (For example, replaying a seed from the Nintendo
+              Switch on PC would result in a completely different layout.)
+            </li>
+            <li>
+              You must play on the latest version of the game, so that other
+              people can easily replay your seeds to verify your runs. (For
+              example, reverting your game to a previous patch before something
+              was changed is not allowed.)
+            </li>
+            <li>
+              Every run must be streamed live on
+              <a
+                href="https://www.twitch.tv"
+                target="_blank"
+                rel="noopener noreferrer">Twitch</a
+              >
+              or
+              <a
+                href="https://www.youtube.com/"
+                target="_blank"
+                rel="noopener noreferrer">YouTube</a
+              >. (For example, showing video recordings of your run after the
+              fact does not count.) You must also
+              <a
+                href="https://streamersplaybook.com/how-to-enable-vod-on-twitch/"
+                >enable VODs</a
+              > so that administrators can review your runs after they are finished.
+            </li>
+          </ul>
+
+          <div class="mb-8" />
+
+          <h3>Before the Run</h3>
+
+          <ul class="section-text">
+            <li>Every run must be played on the "hard" difficulty.</li>
+            <li>
+              Every run must be played on a fully-unlocked save file (i.e. Dead
+              God).
+            </li>
+            <li>
+              Any mods that affect the gameplay are forbidden. (For example,
+              mods that add new items, add new rooms, or change vanilla
+              mechanics.) Mods that are only cosmetic are allowed. (For example,
+              sprite tweaks.)
+            </li>
+            <li>
+              When your streak starts, the donation machine in the shop can be
+              at any value between 0 and 999. However, once the streak starts,
+              you are not allowed to change this value in any way other than by
+              donating coins to the machine and bombing the machine. (For
+              example, you are not allowed to edit the save file in manually
+              change the value.) You are allowed to bomb the donation machine in
+              the shop in order to get coins that were donated in a previous
+              run.
+            </li>
             <li>
               You must start the run as the character from the category you are
-              playing. If you accidently start a run as a different character
+              playing. If you accidentally start a run as a different character
               from the streak you are working on, you are allowed to exit the
               game and restore a previously backed up save file in order to
               preserve the streak count.
             </li>
+            <li>
+              You are not allowed to use the debug console, Cheat Engine, or any
+              other way of modifying the game. (One exception is when you
+              accidentally take The Polaroid or The Negative, and you are in the
+              middle of a streak that requires the other photo. In this case,
+              you are allowed to use the debug console to remove the photo that
+              you took and manually add the photo that you need.)
+            </li>
+          </ul>
+
+          <div class="mb-8" />
+
+          <h3>During the Run</h3>
+
+          <ul class="section-text">
             <li>
               You must trigger the ending cinematic of the goal for the category
               you are playing. (For example, if you complete the goal but die
@@ -55,10 +131,10 @@
               it is counted the same as if you died to the boss. Ending a run at
               Cathedral or Sheol is not allowed.)
             </li>
-            <li>Every run must be played on the "hard" difficulty.</li>
             <li>
-              Every run must be played on a fully-unlocked save file (i.e. Dead
-              God).
+              You are allowed to end the run on another character than the one
+              you started with. (For example, you are allowed to take Judas'
+              Shadow and revive as Dark Judas.)
             </li>
             <li>
               You are not allowed to pause the game in order to gain any type of
@@ -68,35 +144,6 @@
               rolling", delete champions, reset the Holy Mantle, and so on.) You
               are allowed to save and quit and continue if it the only way to
               recover from a softlock such as a bugged room.
-            </li>
-            <li>
-              Every run must be streamed live on
-              <a
-                href="https://www.twitch.tv"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Twitch
-              </a>
-              or
-              <a
-                href="https://www.youtube.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                YouTube
-              </a>. (For example, showing video recordings of your run after the
-              fact does not count.) You must also
-              <a
-                href="https://streamersplaybook.com/how-to-enable-vod-on-twitch/"
-                >enabled VODs</a
-              > so that administrators can review your runs after the fact.
-            </li>
-            <li>
-              Any mods that affect the gameplay are forbidden. (For example,
-              mods that add new items, add new rooms, or change vanilla
-              mechanics.) Mods that are only cosmetic are allowed. (For example,
-              sprite tweaks.)
             </li>
             <li>
               All runs should have the seeds visible on the screen, so that they
@@ -110,37 +157,7 @@
               </a> for this purpose. Otherwise, you can pause at the beginning or
               at the end of your run.)
             </li>
-            <li>
-              You must play on PC, so that other people can replay your seeds to
-              verify your runs. (For example, replaying a seed from the Nintendo
-              Switch on PC would result in a completely different layout.)
-            </li>
-            <li>
-              You must play on the latest version of the game, so that other
-              people can easily replay your seeds to verify your runs. (For
-              example, reverting your game to a previous patch before something
-              was changed is not allowed.)
-            </li>
-            <li>
-              You are allowed to end the run on another character than the one
-              you started with. (For example, you are allowed to take Judas'
-              Shadow and revive as Dark Judas.)
-            </li>
-            <li>
-              When your streak starts, the donation machine in the shop can be
-              at any value between 0 and 999. However, once the streak starts,
-              you are not allowed to change this value in any way other than by
-              donating coins to the machine and bombing the machine. (For
-              example, you are not allowed to edit the save file in manually
-              change the value.) You are allowed to bomb the donation machine in
-              the shop in order to get coins that were donated in a previous
-              run.
-            </li>
-            <li>
-              You are not allowed to use the debug console, Cheat Engine, or any
-              other way of modifying the game.
-            </li>
-          </ol>
+          </ul>
         </div>
       </div>
     </div>
@@ -150,7 +167,7 @@
     <div class="bg-gray-50 shadow sm:rounded-lg sm:overflow-hidden">
       <div class="divide-y divide-gray-200">
         <div class="px-4 py-5 sm:px-6">
-          <h2 class="section-header">Other Links</h2>
+          <h2>Other Links</h2>
           <ul class="section-text">
             <li>
               <a
@@ -197,8 +214,12 @@
 </main>
 
 <style lang="postcss">
-  .section-header {
-    @apply text-3xl font-medium text-gray-900 mb-4;
+  h2 {
+    @apply text-3xl font-medium text-gray-900 mb-8;
+  }
+
+  h3 {
+    @apply text-xl mb-8;
   }
 
   .section-text {
@@ -206,6 +227,6 @@
   }
 
   .section-text > li {
-    @apply list-decimal relative left-5 mb-1;
+    @apply list-disc relative left-5 mb-1;
   }
 </style>
