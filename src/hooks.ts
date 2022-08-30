4f8a2abe68a43a72560a15a3ed65dda748845274
James <5511220+Zamiell@users.noreply.github.com>
2022-08-29 20:34:07 -0400

    refactor: hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,39 +1,42 @@
 import { SESSION_KEY } from "$env/static/private";
-import type { Handle } from "@sveltejs/kit";
+import type { Handle, RequestEvent } from "@sveltejs/kit";
 import * as cookie from "cookie";
 import aes from "crypto-js/aes";
 
 const COOKIE_NAME = "discordAccessToken";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  // Before running the route logic.
-  {
-    const cookieHeader = event.request.headers.get("cookie") ?? "";
-    const cookies = cookie.parse(cookieHeader);
-    const discordAccessTokenEncrypted = cookies[COOKIE_NAME];
+  beforeRouteLogic(event);
 
-    if (
-      // The type definition for "cookie.parse" is bugged, as non-existent cookies will be
-      // undefined.
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      discordAccessTokenEncrypted === undefined ||
-      discordAccessTokenEncrypted === ""
-    ) {
-      event.locals.discordAccessToken = null;
-    } else {
-      const discordAccessToken = aes.decrypt(
-        discordAccessTokenEncrypted,
-        SESSION_KEY,
-      );
-
-      event.locals.discordAccessToken = discordAccessToken.toString();
-    }
-  }
-
-  // Run the route logic.
   const response = await resolve(event);
 
-  // After running the route logic.
+  return afterRouteLogic(event, response);
+};
+
+function beforeRouteLogic(event: RequestEvent) {
+  const cookieHeader = event.request.headers.get("cookie") ?? "";
+  const cookies = cookie.parse(cookieHeader);
+  const discordAccessTokenEncrypted = cookies[COOKIE_NAME];
+
+  if (
+    // The type definition for "cookie.parse" is bugged, as non-existent cookies will be undefined.
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    discordAccessTokenEncrypted === undefined ||
+    discordAccessTokenEncrypted === ""
+  ) {
+    event.locals.discordAccessToken = null;
+    return;
+  }
+
+  const discordAccessToken = aes.decrypt(
+    discordAccessTokenEncrypted,
+    SESSION_KEY,
+  );
+
+  event.locals.discordAccessToken = discordAccessToken.toString();
+}
+
+function afterRouteLogic(event: RequestEvent, response: Response) {
   if (
     event.locals.shouldSetCookie &&
     event.locals.discordAccessToken !== null
@@ -53,4 +56,4 @@ export const handle: Handle = async ({ event, resolve }) => {
   }
 
   return response;
-};
+}
