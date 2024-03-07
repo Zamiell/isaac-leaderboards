091de6e9286b85c32a4b06d692139f5a9c300147
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-03-07 12:47:01 -0500

    fix: lint

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (event) => {
-  // eslint-disable-next-line deprecation/deprecation
   const session = await event.locals.getSession();
   // console.log(session);
   return {
